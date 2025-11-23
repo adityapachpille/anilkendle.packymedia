54b7efcb8d9dedc07d59f5f6285a409bef3863cc
adityapachpille <158599059+adityapachpille@users.noreply.github.com>
2025-11-23 10:36:58 +0530

    Remove footer links from Footer component
    Removed the footer links section from the Footer component.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,12 +4,6 @@ export default function Footer() {
     <footer className="footer">
       <div className="footerInner">
         <p className="footerText">Powered By Ishwari Graphics Mangrulpir.</p>
-        <div className="footerLinks">
-          <Link href="https://wa.me/918806112331" className="footerLink">
-            Design & Develop By ARP Digital Solution.
-          </Link>
-          {/* Add more links if needed */}
-        </div>
       </div>
     </footer>
   );
