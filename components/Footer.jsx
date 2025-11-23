c7c388f93f3db046ab49166ce673e5c998a8f9e8
adityapachpille <158599059+adityapachpille@users.noreply.github.com>
2025-11-23 10:48:11 +0530

    Update footer text to reflect new branding

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footerInner">
-        <p className="footerText">Powered By Ishwari Graphics Mangrulpir.</p>
+        <p className="footerText">Powered By Packy Media Services</p>
       </div>
     </footer>
   );
